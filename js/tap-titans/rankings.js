15fc7c9feeae402299bca8ce0b18280fed836db2
mc <michael.ian.curry@gmail.com>
2016-12-30 12:24:33 -0800

    Add loops for ranking

--- js/tap-titans/rankings.js
+++ js/tap-titans/rankings.js
@@ -1,8 +1,3 @@
-console.clear();
-
-let members,
-    kills
-
 // function: Load member data
 function load_members() {
   return fetch("https://kernelcurry.com/js/tap-titans/members.json")
@@ -17,21 +12,47 @@ function load_kills() {
 
 function init() {
   Promise.all([
-    load_members(),
-    load_kills()
-  ])
-  .then(data => {
-    console.log(data)
-    calculate_rankings(members, kills);
-  })
-  .catch(err => {
-    console.log(err);
-    $("div.tap-titans table.rankings").replaceWith("<h2>ERROR LOADING MEMBER INFORMATION</h2>");   
-    $("div.tap-titans table.rankings").replaceWith("<h2>ERROR LOADING KILL INFORMATION</h2>");
-  });
+      load_members(),
+      load_kills()
+    ])
+    .then(data => {
+      const members = data[0];
+      const kills = data[1];
+      calculate_rankings(members, kills);
+    })
+    .catch(err => {
+      console.log(err);
+      $("div.tap-titans table.rankings").replaceWith("<h2>ERROR LOADING MEMBER INFORMATION</h2>");
+      $("div.tap-titans table.rankings").replaceWith("<h2>ERROR LOADING KILL INFORMATION</h2>");
+    });
 }
+
 function calculate_rankings(members, kills) {
-  // todo: impliment
+  console.log(members, kills);
+
+
+  members.forEach(function(member, m_index) {
+    members[m_index].dkp = 0;
+    console.log("ranking: " + member.username);
+
+    kills.forEach(function(kill, k_index) {
+      // check to see if we should count this kill for this member
+      var join_date = Date.parse(member.joined);
+      var kill_date = Date.parse(kill.date);
+      if (join_date > kill_date) {
+        return;
+      }
+      
+      kill.damage.forEach(function(damage, d_index) {
+        if (damage.username.match(/kernelcurry/i)) {
+          console.log(damage);
+        }
+      });
+    });
+
+    console.log(member.username + " : " + member.dkp);
+  });
+
 }
 
 init();
