9c2001593f511f1482c72977ddc8ba07b47056b5
mc <michael.ian.curry@gmail.com>
2016-12-30 11:07:43 -0800

    Init ranking script (get json files into arrays)

--- js/tap-titans/rankings.js
+++ js/tap-titans/rankings.js
@@ -1,1 +1,37 @@
-rankings.js
+console.clear();
+
+let members,
+    kills
+
+// function: Load member data
+function load_members() {
+  return fetch("https://kernelcurry.com/js/tap-titans/members.json")
+    .then(res => res.json());
+}
+
+// function: Load kill data
+function load_kills() {
+  return fetch("https://kernelcurry.com/js/tap-titans/kills.json")
+    .then(res => res.json())
+}
+
+function init() {
+  Promise.all([
+    load_members(),
+    load_kills()
+  ])
+  .then(data => {
+    console.log(data)
+    calculate_rankings(members, kills);
+  })
+  .catch(err => {
+    console.log(err);
+    $("div.tap-titans table.rankings").replaceWith("<h2>ERROR LOADING MEMBER INFORMATION</h2>");   
+    $("div.tap-titans table.rankings").replaceWith("<h2>ERROR LOADING KILL INFORMATION</h2>");
+  });
+}
+function calculate_rankings(members, kills) {
+  // todo: impliment
+}
+
+init();
