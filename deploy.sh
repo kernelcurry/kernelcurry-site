8235f30b0e114a6573333b0d6483266be1a939a8
michael curry <michael.ian.curry@gmail.com>
2017-06-24 09:24:40 -0700

    Syntax changes to deploy.sh to allow for easy viewing

--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,33 @@
 
 if [[ $(git status -s) ]]
 then
-    echo "The working directory is dirty. Please commit any pending changes."
+    echo "***The working directory is dirty. Please commit any pending changes.***"
     exit 1;
 fi
 
+echo "---------------------------------------------------------------"
 echo "Deleting old publication"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
+echo "---------------------------------------------------------------"
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public upstream/gh-pages
 
+echo "---------------------------------------------------------------"
 echo "Removing existing files"
 rm -rf public/*
 
+echo "---------------------------------------------------------------"
 echo "Generating site"
 hugo
 
+echo "---------------------------------------------------------------"
 echo "Generating CNAME file"
 cp CNAME public/CNAME
 
+echo "---------------------------------------------------------------"
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && cd ..
